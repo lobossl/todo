65e9ef242af9fbb3e7168411abf7a6a2f86eaef3
Lobo <triohouse@hotmail.com>
2023-09-11 12:10:29 +0200

    new
    new

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
-//Hey you like to check my source code? well, enjoy =)
-import { database } from "./modules/lobo.js"
+class database{
+	constructor(dbname){
+        	this.dbname = dbname;
+        	this.read = JSON.parse(localStorage.getItem(dbname)) || []
+	}
+
+	load(){
+		let test = this.read.sort((a, b) => b.date - a.date)
+		return test
+	}
+
+	save(obj){
+		this.read.push(obj)
+		localStorage.setItem(this.dbname, JSON.stringify(this.read))
+	}
+
+	saveAll(){
+		localStorage.setItem(this.dbname, JSON.stringify(this.read))
+	}
+
+	clearAll(){
+		localStorage.clear()
+	}
+
+	restore(obj){
+		localStorage.setItem(this.dbname, JSON.stringify(JSON.parse(obj)))
+	}
+
+	backup(){
+		return JSON.stringify(this.read)
+	}
+}
 
 let db = new database("lobo")
 
 window.addEventListener("load",() =>{
 	reload()
-	document.documentElement.spellcheck = false;
+	document.documentElement.spellcheck = false
 })
 
 document.getElementById("create").addEventListener("click",() =>{
 	db.save({
-		color: "",
-      		id: "",
-      		date: setDate(),
-      		text: "",
-		tagged: false
-    	})
+		id: "",
+		date: setDate(),
+		text: ""
+    })
 
-    	reload()
+    reload()
 	document.getElementById("details").open = false
 })
 
@@ -29,8 +57,7 @@ document.getElementById("restore").addEventListener("click",() =>{
     	{
         	const file = event.target.files[0]
 
-        	if(file)
-        	{
+        	if(file){
             		const reader = new FileReader()
 
             		reader.onload = function(event)
@@ -76,126 +103,74 @@ document.getElementById("clear").addEventListener("click",() =>{
 })
 
 document.addEventListener("click",(e) =>{
-	if(e.target.id == "createTagged")
-	{
-		db.load().forEach((get) =>
-		{
-			if(get.id == e.target.setId)
-			{
-				if(get.tagged == false)
-				{
-					get.color = "#f37636"
-					get.tagged = true
-				}
-				else
-				{
-					get.color = "#444"
-					get.tagged = false
-				}
-				db.saveAll()
-				reload()
-			}
-		})
-	}
-
-	if(e.target.id == "createRemove")
-	{
-		try
-		{
+	if(e.target.id == "createRemove"){
+		try{
 			db.load().splice(e.target.setId,1)
 			db.saveAll()
 			reload()
 		}
-		catch(err)
-		{
+		catch(err){
 			console.log("error: createRemove failed")
 		}
 	}
 })
 
 document.addEventListener("keyup",(e) =>{
-	if(e.target.id == "createText")
-	{
-		db.load().forEach((get) =>
-		{
-			if(get.id == e.target.setId)
-			{
+	if(e.target.id == "createText"){
+		db.load().forEach((get,index) =>{
+			if(get.id == e.target.setId){
 				get.text = e.target.innerText
 				get.date = setDate()
+				db.saveAll()
 			}
 		})
-
-		db.saveAll()
 	}
 })
 
 function reload(){
 	document.getElementById("main").innerText = ""
 
-    	try
-    	{
-        	db.load().forEach((e,index) =>
-        	{
-            		let createBox = document.createElement("p")
-            		createBox.id = "createBox"
-            		createBox.className = "createBox"
+    	try{
+        	db.load().forEach((e,index) =>{
+				let createBox = document.createElement("p")
+				createBox.id = "createBox"
+				createBox.className = "createBox"
 
-			let createButtons = document.createElement("div")
-			createButtons.className = "createButtons"
+				let left = document.createElement("div")
+				left.className = "left"
 
-			let createTagged = document.createElement("button")
-			createTagged.id = "createTagged"
-			createTagged.className = "btn"
-			createTagged.innerText = "TAG"
-			createTagged.setId = index
+				let right = document.createElement("div")
+				right.className = "right"
+                
+				let createText = document.createElement("p")
+				createText.spellcheck = false
+				createText.id = "createText"
+				createText.className = "createText"
+				createText.contentEditable = "true"
+				createText.setId = index
+				createText.innerText = e.text
+				e.id = index
 
-            		let createText = document.createElement("p")
-			createText.spellcheck = false
-            		createText.id = "createText"
-            		createText.className = "createText"
-            		createText.contentEditable = "true"
-            		createText.setId = index
-            		createText.innerText = e.text
-            		e.id = index
-			createText.style.borderColor = e.color
+				let createRemove = document.createElement("button")
+				createRemove.id = "createRemove"
+				createRemove.className = "btn"
+				createRemove.innerText = "X"
+				createRemove.setId = index
 
-            		let createDate = document.createElement("div")
-            		createDate.id = "createDate"
-            		createDate.className = "createDate"
-            		createDate.innerText = e.date
-
-            		let createRemove = document.createElement("button")
-            		createRemove.id = "createRemove"
-            		createRemove.className = "btn"
-            		createRemove.innerText = "DELETE"
-            		createRemove.setId = index
-
-			document.getElementById("main").append(createBox)
-			createBox.append(createDate)
-			createBox.append(createButtons)
-			createButtons.append(createRemove)
-			createButtons.append(createTagged)
-			createBox.append(createText)
+				document.getElementById("main").append(createBox)
+				createBox.append(left)
+				createBox.append(right)
+				left.append(createText)
+				right.append(createRemove)
         	})
     	}
-    	catch(err)
-    	{
+    	catch(err){
         	console.log("error: no database found")
     	}
 }
 
 function setDate(){
-	const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
-	const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+	const dato = Date.now()
 
-	const d = new Date()
-
-	let name = months[d.getMonth()]
-	let year = d.getFullYear()
-	let day = days[d.getDay()]
-	let month = d.getDate()
-
-	let final = day + " " + month + ". " + name + " " + year
-
-	return final
+	return dato
 }
