70b23ff1f55095a8f1f82072c814d22bf6d1aa2a
Lobo <triohouse@hotmail.com>
2023-12-02 17:40:18 +0100

    1.0.4
    1.0.4

--- app.js
+++ app.js
@@ -1,135 +1,145 @@
-//1.0.3
-let localStorageDataBaseName = "lobo"
+//1.0.4
 
-if(location.protocol == "http:") {
+if(location.protocol == "http:")
+{
 	location.href = location.href.replace("http://", "https://");
 }
 
-class database {
-	constructor(dbname) {
+class myProject {
+	constructor(dbname)
+	{
+			this.elementBoxes = document.getElementById("boxes")
+			this.elementDelete = document.getElementById("delete")
+			this.elementAddBtn = document.getElementById("addButton")
+			this.inputText = document.getElementById("inputText")
         	this.dbname = dbname;
-        	this.read = JSON.parse(localStorage.getItem(dbname)) || []
+        	this.dbread = JSON.parse(localStorage.getItem(dbname)) || []
 	}
 
-	load() {
-		let getArray = this.read
-		return getArray
+	SAVE(obj)
+	{
+		this.dbread.push(obj)
+		localStorage.setItem(this.dbname, JSON.stringify(this.dbread))
 	}
 
-	save(obj) {
-		this.read.push(obj)
-		localStorage.setItem(this.dbname, JSON.stringify(this.read))
+	SAVEALL()
+	{
+		localStorage.setItem(this.dbname, JSON.stringify(this.dbread))
 	}
 
-	clear() {
-		this.read = []
-	}
-
-	saveAll() {
-		localStorage.setItem(this.dbname, JSON.stringify(this.read))
-	}
-
-	restore(obj) {
-		localStorage.setItem(this.dbname, JSON.stringify(JSON.parse(obj)))
-	}
-
-	backup() {
-		return JSON.stringify(this.read)
-	}
-}
-
-let db = new database(localStorageDataBaseName)
-
-function loadResult()
-{
-	document.getElementById("boxes").innerText = ""
-
-	db.load().forEach((e,index) => {
-		e.ident = index
-
-		let boxes = document.getElementById("boxes")
-		let createNewBox = document.createElement("div")
-		let createRadio = document.createElement("input")
-		let createLabel = document.createElement("label")
-
-		createRadio.type = "checkbox"
-		createRadio.name = e.text
-		createRadio.value = e.text
-		createRadio.ident = index
-		createRadio.underline = e.underline
-		createRadio.className = "margin-def"
-
-		createLabel.innerText = e.text
-
-		boxes.appendChild(createNewBox)
-
-		createNewBox.appendChild(createRadio)
-		createNewBox.appendChild(createLabel)
-
-		if(e.underline == true)
+	DELETE()
+	{
+		for(let i = this.dbread.length - 1; i >= 0; i--)
 		{
-			createLabel.style.textDecoration = "line-through red"
-			createLabel.style.color = "#CCCCCC"
+			if(this.dbread[i].underline == true)
+			{
+				this.dbread.splice(i,1)
+			}
+		}
+
+		this.SAVEALL()
+		this.RELOAD()
+	}
+
+	ADD()
+	{
+		if(this.inputText.value.length > 0)
+		{
+			let monthNames = ["January", "February", "March", "April", "May","June","July", "August", "September", "October", "November","December"]
+			let d = new Date()
+	
+			this.SAVE({
+				ident: null,
+				underline: false,
+				date: monthNames[d.getMonth()],
+				text: this.inputText.value
+			})
+	
+			this.RELOAD()
+			
+			this.inputText.value = ""
+		}
+	}
+
+	RELOAD()
+	{
+		this.elementBoxes.innerText = ""
+
+		for(let i = this.dbread.length - 1; i >= 0; i--)
+		{
+			this.dbread[i].ident = i
+
+			let createNewBox = document.createElement("div")
+			let createRadio = document.createElement("input")
+			let createLabel = document.createElement("label")
+
+			createRadio.type = "checkbox"
+			createRadio.id = "checkbox"
+			createRadio.name = this.dbread[i].text
+			createRadio.value = this.dbread[i].text
+			createRadio.ident = i
+			createRadio.underline = this.dbread[i].underline
+			createRadio.className = "margin-def"
+
+			createLabel.innerText = this.dbread[i].text
+
+			this.UNDERLINE(this.dbread[i].underline,createLabel,createRadio)
+
+			this.elementBoxes.appendChild(createNewBox)
+
+			createNewBox.appendChild(createRadio)
+			createNewBox.appendChild(createLabel)
+		}
+	}
+
+	UNDERLINE(e,label,radio)
+	{
+		if(e == true)
+		{
+			radio.checked = true
+			label.style.textDecoration = "line-through red"
 		}
 		else
 		{
-			createLabel.style.textDecoration = "none"
-			createLabel.style.color = "#888888"
-		}
-
-		//
-		createRadio.addEventListener("click",(x) =>
-		{
-			if(x.target.underline)
-			{
-				db.load()[x.target.ident].underline = false
-			}
-			else
-			{
-				db.load()[x.target.ident].underline = true
-			}
-
-			db.saveAll()
-
-			loadResult()
-		})
-	})
-}
-
-document.getElementById("delete").addEventListener("click",() =>
-{
-	let myDatabase = db.load()
-
-	for (let i = myDatabase.length - 1; i >= 0; i--) {
-		if (myDatabase[i].underline === true)
-		{
-			myDatabase.splice(i, 1);
+			radio.checked = false
+			label.style.textDecoration = "none"
 		}
 	}
 
-	db.saveAll()
-	loadResult()
-})
-
-document.getElementById("addButton").addEventListener("click",(e) => {
-	if(document.getElementById("inputText").value.length > 0)
+	SELECT(e)
 	{
-		let monthNames = ["January", "February", "March", "April", "May","June","July", "August", "September", "October", "November","December"]
-		let d = new Date()
+		if(this.dbread[e].underline == true)
+		{
+			this.dbread[e].underline = false
+		}
+		else
+		{
+			this.dbread[e].underline = true
+		}
 
-		db.save({
-			ident: null,
-			underline: false,
-			date: monthNames[d.getMonth()],
-			text: document.getElementById("inputText").value
-		})
-
-		loadResult()
-		
-		document.getElementById("inputText").value = ""
+		this.SAVEALL()
 	}
+}
+
+let Project = new myProject("test")
+
+Project.RELOAD()
+
+Project.elementDelete.addEventListener("click",(e) =>
+{
+	Project.DELETE()
 })
 
-window.onload = () => {
-	loadResult()
-}
+Project.elementAddBtn.addEventListener("click",(e) =>
+{
+	Project.ADD()
+})
+
+document.addEventListener("click",(e) =>
+{
+	if(e.target.id == "checkbox")
+	{
+		Project.SELECT(e.target.ident)
+		Project.RELOAD()
+	}
+})
